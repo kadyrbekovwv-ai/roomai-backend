f60021bc4fe96d03cf6f7883a47da627870e1df0
kadyrbekovwv-ai <kadyrbekovwv@gmail.com>
2026-05-13 13:41:39 +0600

    Update model endpoint in proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
       const { prompt } = req.body;
 
       const response = await fetch(
-        'https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-schnell',
+        'https://api-inference.huggingface.co/models/stabilityai/sdxl-turbo',
         {
           method: 'POST',
           headers: {
